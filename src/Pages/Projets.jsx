16f28d4c064e24042a304e178d594e5e294f5dee
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-08 18:40:46 +0530

    added all the images of the projects

--- src/Pages/Projets.jsx
+++ src/Pages/Projets.jsx
@@ -16,7 +16,7 @@ const Projets = () => {
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -28,7 +28,7 @@ const Projets = () => {
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -40,11 +40,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -52,39 +52,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="px-4">
-        <div className="w-full h-[400px] mb-3 flex gap-3">
-          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -96,11 +68,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -108,39 +80,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="px-4">
-        <div className="w-full h-[400px] mb-3 flex gap-3">
-          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -152,11 +96,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/BEST/BEST_site_menu_Thumbnail-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -164,39 +108,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="px-4">
-        <div className="w-full h-[400px] mb-3 flex gap-3">
-          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
-              alt=""
-            />
-            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -208,11 +124,11 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
@@ -220,11 +136,95 @@ const Projets = () => {
           <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg"
+              src="https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg"
               alt=""
             />
             <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
-              <h2 className="uppercase text-[4vw] font-[font-2] border-4 rounded-full pt-2 px-2">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
+                Voir le projet
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-4">
+        <div className="w-full h-[400px] mb-3 flex gap-3">
+          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              src="https://k72.ca/uploads/caseStudies/OSM/thumbnail_OSM_1280-1280x960.jpg"
+              alt=""
+            />
+            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
+                Voir le projet
+              </h2>
+            </div>
+          </div>
+          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              src="https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg"
+              alt=""
+            />
+            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
+                Voir le projet
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-4">
+        <div className="w-full h-[400px] mb-3 flex gap-3">
+          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              src="https://k72.ca/uploads/caseStudies/CRISIS24/crisis24_behance_1920X1200_cartes-1280x960.jpg"
+              alt=""
+            />
+            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
+                Voir le projet
+              </h2>
+            </div>
+          </div>
+          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              src="https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg"
+              alt=""
+            />
+            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
+                Voir le projet
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-4">
+        <div className="w-full h-[400px] mb-3 flex gap-3">
+          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              src="https://k72.ca/uploads/caseStudies/PME-MTL/PME-MTL_Thumbnail-1280x960.jpg"
+              alt=""
+            />
+            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
+                Voir le projet
+              </h2>
+            </div>
+          </div>
+          <div className="w-1/2 h-full group transition-all flex items-center justify-center relative rounded-none hover:rounded-[50px] overflow-hidden">
+            <img
+              className="h-full w-full object-cover"
+              src="https://k72.ca/uploads/caseStudies/FRUITE/Fruite_thumbnail_bbq-1280x960.jpg"
+              alt=""
+            />
+            <div className="opacity-0 transition-all group-hover:opacity-100 absolute flex items-center justify-center">
+              <h2 className="uppercase text-[4vw] font-[font-2] border-3 rounded-full pt-2 px-2">
                 Voir le projet
               </h2>
             </div>
