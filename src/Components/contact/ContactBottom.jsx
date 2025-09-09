140fa89d7dc508c1a877f775c40587e14d0951e9
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-10 00:41:50 +0530

    linked all the social media links

--- src/Components/contact/ContactBottom.jsx
+++ src/Components/contact/ContactBottom.jsx
@@ -6,19 +6,27 @@ const ContactBottom = () => {
       <div className="uppercase text-center mb-5 text-[1.1vw] font-[font-2]">
         <p>Suivez-nous</p>
       </div>
-      <div className="gap-2 flex justify-center uppercase text-center text-[5vw] font-[font-2]">
-        <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
-          fb
-        </h1>
-        <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
-          ig
-        </h1>
-        <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
-          in
-        </h1>
-        <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
-          be
-        </h1>
+      <div className="pb-10 gap-2 flex justify-center uppercase text-center text-[5vw] font-[font-2]">
+        <a href="https://www.facebook.com/K72.ca/">
+          <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+            fb
+          </h1>
+        </a>
+        <a href="https://www.instagram.com/k72_creation/">
+          <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+            ig
+          </h1>
+        </a>
+        <a href="https://www.linkedin.com/company/k72">
+          <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+            in
+          </h1>
+        </a>
+        <a href="https://www.behance.net/agenceK72">
+          <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+            be
+          </h1>
+        </a>
       </div>
     </div>
   );
