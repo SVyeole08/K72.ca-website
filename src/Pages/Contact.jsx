4ae4ff49367b133ad3694a5b6df2c28b4a9c0be9
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-09 19:13:18 +0530

    complete animation

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,19 +7,44 @@ const Contact = () => {
       <ContactTop />
       <div
         ref={ContactDiv}
-        className="moveX w-[500vw] whitespace-nowrap transition-all-ease flex items-center justify-center text-black bg-[#D3FD50] h-[120px] pt-2 absolute top-150 origin-center"
+        className=" w-fit transition-all-ease flex items-center justify-center text-black bg-[#D3FD50] h-[120px] pt-2 absolute top-150 origin-center"
       >
-        <div className="items-center flex">
-          <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-            bonjour@k72.ca&nbsp;
-          </h1>
-          <img className="h-25" src="src\Media\pixcar.png" alt="" />
-        </div>
-        <div className="items-center flex">
-          <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-            bonjour@k72.ca&nbsp;
-          </h1>
-          <img className="h-25" src="src\Media\pixcar.png" alt="" />
+        <div className="moveX whitespace-nowrap flex items-center">
+          <div className="items-center flex">
+            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+              &nbsp;bonjour@k72.ca
+            </h1>
+            <img className="h-25" src="src\Media\pixcar.png" alt="" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className="items-center flex">
+            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+              &nbsp;bonjour@k72.ca
+            </h1>
+            <img className="h-25" src="src\Media\pixcar.png" alt="" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className="items-center flex">
+            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+              &nbsp;bonjour@k72.ca
+            </h1>
+            <img className="h-25" src="src\Media\pixcar.png" alt="" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className="items-center flex">
+            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+              &nbsp;bonjour@k72.ca
+            </h1>
+            <img className="h-25" src="src\Media\pixcar.png" alt="" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className="items-center flex">
+            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+              &nbsp;bonjour@k72.ca
+            </h1>
+            <img className="h-25" src="src\Media\pixcar.png" alt="" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
         </div>
       </div>
     </>
