a05651a5d2069a2483dcc13ce737d5b67e07d60c
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-10 00:58:08 +0530

    Done all the stuff of Contact component

--- src/Components/contact/ContactBottom.jsx
+++ src/Components/contact/ContactBottom.jsx
@@ -7,22 +7,22 @@ const ContactBottom = () => {
         <p>Suivez-nous</p>
       </div>
       <div className="pb-10 gap-2 flex justify-center uppercase text-center text-[5vw] font-[font-2]">
-        <a href="https://www.facebook.com/K72.ca/">
+        <a target="_blank" href="https://www.facebook.com/K72.ca/">
           <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
             fb
           </h1>
         </a>
-        <a href="https://www.instagram.com/k72_creation/">
+        <a target="_blank" href="https://www.instagram.com/k72_creation/">
           <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
             ig
           </h1>
         </a>
-        <a href="https://www.linkedin.com/company/k72">
+        <a target="_blank" href="https://www.linkedin.com/company/k72">
           <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
             in
           </h1>
         </a>
-        <a href="https://www.behance.net/agenceK72">
+        <a target="_blank" href="https://www.behance.net/agenceK72">
           <h1 className="border-1 rounded-full w-[9vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
             be
           </h1>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,53 +1,50 @@
-import { useRef } from "react";
 import ContactTop from "../Components/contact/ContactTop";
 import ContactBottom from "../Components/contact/ContactBottom";
 const Contact = () => {
-  const ContactDiv = useRef(null);
   return (
     <>
       <ContactTop />
-      <div
-        ref={ContactDiv}
-        className=" w-fit transition-all-ease flex items-center justify-center text-black bg-[#D3FD50] h-[120px] pt-2 absolute top-150 origin-center"
-      >
-        <div className="moveX whitespace-nowrap flex items-center">
-          <div className="items-center flex">
-            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-              &nbsp;bonjour@k72.ca
-            </h1>
-            <img className="h-25" src="src\Media\pixcar.png" alt="" />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </div>
-          <div className="items-center flex">
-            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-              &nbsp;bonjour@k72.ca
-            </h1>
-            <img className="h-25" src="src\Media\pixcar.png" alt="" />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </div>
-          <div className="items-center flex">
-            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-              &nbsp;bonjour@k72.ca
-            </h1>
-            <img className="h-25" src="src\Media\pixcar.png" alt="" />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </div>
-          <div className="items-center flex">
-            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-              &nbsp;bonjour@k72.ca
-            </h1>
-            <img className="h-25" src="src\Media\pixcar.png" alt="" />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </div>
-          <div className="items-center flex">
-            <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
-              &nbsp;bonjour@k72.ca
-            </h1>
-            <img className="h-25" src="src\Media\pixcar.png" alt="" />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="mailto:bonjour@k72.ca" target="_blank">
+        <div className=" w-fit transition-all-ease flex items-center justify-center text-black bg-[#D3FD50] h-[120px] pt-2 absolute top-150 origin-center">
+          <div className="moveX whitespace-nowrap flex items-center">
+            <div className="items-center flex">
+              <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+                &nbsp;bonjour@k72.ca
+              </h1>
+              <img className="h-25" src="src\Media\pixcar.png" alt="" />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            <div className="items-center flex">
+              <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+                &nbsp;bonjour@k72.ca
+              </h1>
+              <img className="h-25" src="src\Media\pixcar.png" alt="" />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            <div className="items-center flex">
+              <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+                &nbsp;bonjour@k72.ca
+              </h1>
+              <img className="h-25" src="src\Media\pixcar.png" alt="" />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            <div className="items-center flex">
+              <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+                &nbsp;bonjour@k72.ca
+              </h1>
+              <img className="h-25" src="src\Media\pixcar.png" alt="" />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            <div className="items-center flex">
+              <h1 className="text-[18vh] pt-2 uppercase font-[font-2]">
+                &nbsp;bonjour@k72.ca
+              </h1>
+              <img className="h-25" src="src\Media\pixcar.png" alt="" />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
           </div>
         </div>
-      </div>
+      </a>
       <ContactBottom />
     </>
   );
