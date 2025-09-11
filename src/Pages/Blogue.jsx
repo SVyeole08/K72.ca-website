dfb8e83187c0bf2c03a3e7ca2cd3ce487981a12c
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-11 15:49:33 +0530

    Done all the stuff of Blogue.jsx

--- src/Pages/Blogue.jsx
+++ src/Pages/Blogue.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Blgcont from "../Components/blogue/Blgcont";
+import { useNavigate } from "react-router-dom";
 
 const Blogue = () => {
+  const navigator = useNavigate();
   return (
     <>
       <div>
@@ -30,6 +32,46 @@ const Blogue = () => {
         <div className="w-[95vw] origin-center h-[0.5px] -mt-66 bg-black ml-[1.5vw]"></div>
       </div>
       <Blgcont />
+      <div className="w-screen transition-all-ease  h-[62vh] flex font-[font-2] justify-between bg-black p-2 mt-60 text-[5vw] text-center">
+        <div className="flex gap-2 uppercase ">
+          <a target="_blank" href="https://www.facebook.com/K72.ca/">
+            <h1 className="border-2 rounded-full w-[8.8vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+              fb
+            </h1>
+          </a>
+          <a target="_blank" href="https://www.instagram.com/k72_creation/">
+            <h1 className="border-2 rounded-full w-[8.8vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+              ig
+            </h1>
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/company/k72">
+            <h1 className="border-2 rounded-full w-[8.8vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+              in
+            </h1>
+          </a>
+          <a target="_blank" href="https://www.behance.net/agenceK72">
+            <h1 className="border-2 rounded-full w-[8.8vw] hover:text-[#D3FD50] hover:border-[#D3FD50] h-[9vh] leading-[5vw] pt-1 ">
+              be
+            </h1>
+          </a>
+        </div>
+        <div
+          className="rounded-full transition-all-ease flex text-center group hover:text-[#D3FD50] h-[9vh] leading-[5vw] pt-1 border-2 px-5 w-fit hover:border-[#D3FD50]"
+          onClick={() => navigator("/contact")}
+        >
+          <h1 className="uppercase">contact</h1>
+          <svg
+            className="pt-1 group-hover:fill-[#D3FD50]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="4.5vw"
+            height="4.5vw"
+            fill="white"
+          >
+            <path d="M256 448L64 192V96l64-96 128 160L384 0l64 96v96L256 448z" />
+          </svg>
+        </div>
+      </div>
     </>
   );
 };
